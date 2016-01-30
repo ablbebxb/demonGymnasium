2b049db949d32e5a56da13297b8265f5da7f3ff3
William Johnson <johnsonalex1@gatech.edu>
2016-01-29 21:01:29 -0500

    Basic setup for players and entities

---
+++ DemonGymnasium/Assets/Scripts/entities/Entity.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entity : MonoBehaviour {
+
+	/*
+	 * Default settings
+	 */
+	public int startingActionPoints;
+
+
+	private int actionPoints;
+
+	// Use this for initialization
+	void Start () {
+		actionPoints = startingActionPoints;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ DemonGymnasium/Assets/Scripts/entities/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Entity {
+
+	/*
+	 * Default settings
+	 */
+	public int startingHP;
+
+	private int HP;
+
+	// Use this for initialization
+	void Start () {
+		HP = startingHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
