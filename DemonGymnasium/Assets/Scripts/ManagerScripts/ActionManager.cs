6c1f819f92509ca8112529635f9a2fa11ca8b7ec
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-01-31 09:06:37 -0500

    actionManager updated

--- DemonGymnasium/Assets/Scripts/ManagerScripts/ActionManager.cs
+++ DemonGymnasium/Assets/Scripts/ManagerScripts/ActionManager.cs
@@ -6,12 +6,91 @@ public class ActionManager : MonoBehaviour {
     public const int SHOOT = 1;
     public const int EXPAND = 2;
 
-    int currentAction;
+    int currentActionSelected;
+    Entity currentEntity;
+    PlayerSelectManager playerSelectManager;
     
     void Start()
     {
-        currentAction = -1;
+        currentActionSelected = -1;
+        playerSelectManager = GetComponent<PlayerSelectManager>();
     }
 
+
+    public void sestActionType(int actionType)
+    {
+        this.currentActionSelected = actionType;
+    }
+
+    public void handleTileSelected()
+    {
+
+    }
+
+    public void selectMovement(Entity entity)
+    {
+        currentEntity = entity;
+        currentActionSelected = MOVING;
+    }
+
+    public void setAttack(Entity entity)
+    {
+        currentEntity = entity;
+        currentActionSelected = SHOOT;
+    }
+
+    public void selectExpand(Entity entity)
+    {
+        currentEntity = entity;
+        currentActionSelected = EXPAND;
+
+        handleExpandLogic();
+    }
+
+    public void handleMovementLogic()
+    {
+        
+    }
+
+    public void handleAttackLogic(Entity entity)
+    {
+        
+    }
+
+    public void handleExpandLogic()
+    {
+        Tile[,] mapTiles = MapGenerator.mapTiles;
+        int x = currentEntity.getCurrentTile().getX();
+        int y = currentEntity.getCurrentTile().getY();
+        mapTiles[x, y].setTileType(currentEntity.entityType);
+        if (x - 1 >= 0)
+        {
+            mapTiles[x - 1, y].setTileType(currentEntity.entityType);
+        }
+        if (y - 1 >= 0)
+        {
+            mapTiles[x, y - 1].setTileType(currentEntity.entityType);
+        }
+        if (x + 1 < mapTiles.GetLength(0))
+        {
+            mapTiles[x + 1, y].setTileType(currentEntity.entityType);
+        }
+        if (y + 1 < mapTiles.GetLength(1))
+        {
+            mapTiles[x, y + 1].setTileType(currentEntity.entityType);
+        }
+
+
+    }
+
+    void checkKillEnemy(Tile tile)
+    {
+        Entity entity = tile.getCurrentEntity();
+         if (entity == null)
+        {
+            return;
+        }
+        
+    } 
 
 }
