5ccb56e3095b5d39d7e07dcd0f37b794ba787d9d
Peilin Li <lipeilin@gatech.edu>
2016-01-31 08:48:29 -0500

    main menu audio implemented

--- DemonGymnasium/Assets/MainMenu_UI.cs
+++ DemonGymnasium/Assets/MainMenu_UI.cs
@@ -4,6 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu_UI : MonoBehaviour {
 
+	AudioSource PlayBtnAS;
+	AudioSource CreditBtnAS;
+	AudioSource QuitBtnAS;
+
+	public AudioClip hoveringSFX;
+	public AudioClip clickingSFX;
+
+	void Awake(){
+		PlayBtnAS = ((RectTransform)transform).Find ("PlayBtn").gameObject.GetComponent<AudioSource> ();
+		CreditBtnAS = ((RectTransform)transform).Find ("CreditBtn").gameObject.GetComponent<AudioSource> ();
+		QuitBtnAS = ((RectTransform)transform).Find ("QuitBtn").gameObject.GetComponent<AudioSource> ();
+	}
+
 	// Use this for initialization
 	void Start () {
 	
@@ -15,11 +28,28 @@ public class MainMenu_UI : MonoBehaviour {
 	}
 
 	public void PlayBtnClicked(){
+		PlayBtnAS.clip = clickingSFX;
+		PlayBtnAS.Stop ();
+		PlayBtnAS.Play ();
 		SceneManager.LoadScene ("MainScene");
 	}
 
+	public void PlayBtnHovered(){
+		PlayBtnAS.clip = hoveringSFX;
+		PlayBtnAS.Stop ();
+		PlayBtnAS.Play ();
+	}
+
 	public void CreditBtnClicked(){
-		
+		CreditBtnAS.clip = clickingSFX;
+		CreditBtnAS.Stop ();
+		CreditBtnAS.Play ();
+	}
+
+	public void CreditBtnHovered(){
+		CreditBtnAS.clip = hoveringSFX;
+		CreditBtnAS.Stop ();
+		CreditBtnAS.Play ();
 	}
 
 	public void QuitBtnClicked(){
