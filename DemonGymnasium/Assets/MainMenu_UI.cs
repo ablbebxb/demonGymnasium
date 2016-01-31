1c160c352daed1d544da4153dd884defb039f557
Peilin Li <lipeilin@gatech.edu>
2016-01-31 07:19:07 -0500

    main menu and end game UI done

---
+++ DemonGymnasium/Assets/MainMenu_UI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_UI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayBtnClicked(){
+		SceneManager.LoadScene ("MainScene");
+	}
+
+	public void CreditBtnClicked(){
+		
+	}
+
+	public void QuitBtnClicked(){
+		Application.Quit ();
+	}
+
+}
